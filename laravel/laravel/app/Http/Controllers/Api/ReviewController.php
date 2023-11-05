3b2c6071ab21996e790f6c47ba030b2a2a7ab475
github myoshiyama <masachihomina@gmail.com>
2023-11-05 11:03:45 +0900

    herokuデプロイ途中

--- laravel/laravel/app/Http/Controllers/Api/ReviewController.php
+++ laravel/laravel/app/Http/Controllers/Api/ReviewController.php
@@ -21,16 +21,16 @@ class ReviewController extends Controller
         DB::beginTransaction();
         try {
             $data = $request->validated();
-    
+
             $booking = Booking::findByReviewKey($data['id']);
-    
+
             if(null === $booking){
                 return abort(404);
             }
-    
+
             $booking->review_key = '';
             $booking->save();
-    
+
             $review = Review::make($data);
             $review->booking_id = $booking->id;
             $review->bookable_id = $booking->bookable_id;
